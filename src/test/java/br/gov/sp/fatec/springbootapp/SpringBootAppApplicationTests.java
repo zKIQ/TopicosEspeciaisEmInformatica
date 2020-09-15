432dc14c6182a97e0362a01e7e3b827e7b41d8e6
zKIQ <caique.santos15@fatec.sp.gov.br>
2020-09-15 13:08:52 +0000

    JPA1

--- src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
@@ -1,13 +1,34 @@
 package br.gov.sp.fatec.springbootapp;
 
+
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.gov.sp.fatec.springbootapp.entity.Usuario;
+import br.gov.sp.fatec.springbootapp.repository.UsuarioRepository;
 
 @SpringBootTest
+@Transactional
+@Rollback
 class SpringBootAppApplicationTests {
+
+    @Autowired    
+    private UsuarioRepository usuarioRepo;
 
 	@Test
 	void contextLoads() {
-	}
+    }
 
+    @Test
+    void testaInsercao(){
+        Usuario usuario = new Usuario();
+        usuario.setNome("Usuario");
+        usuario.setSenha("SenhaMuit0");
+        usuarioRepo.save(usuario);
+        assertNotNull(usuario.getId());
+    }  
 }
